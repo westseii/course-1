70086f3d7a9d9dd1984a501b8d72b9e844dc5526
Scott West <zabmagi@Scotts-MacBook-Pro.local>
2021-01-05 18:36:53 -0600

    new repo

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <h2>{{ bookNum }} {{ bookTitles.title }}</h2>
+</template>
+
+<script>
+import { ref, reactive } from "vue";
+
+export default {
+  setup() {
+    const bookNum = ref("Scooter");
+    const bookTitles = reactive({
+      title: "bob's bobs",
+      title2: "bob's bobs 2: the bobbening",
+    });
+    return {
+      bookNum,
+      bookTitles,
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
